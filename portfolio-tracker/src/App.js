d6ff25045d3ca87279dbf450d7257159c99b4258
Freddy <freddydordoni@Mac-mini.local>
2023-08-16 10:26:07 +1000

    worked the header component, wrapped in ThemeProvider and darkmode, and added currencies

--- portfolio-tracker/src/App.js
+++ portfolio-tracker/src/App.js
@@ -5,18 +5,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./Pages/HomePage";
 import Coinpage from "./Pages/CoinPage";
+import { StyledEngineProvider } from "@mui/material/styles"; 
+import { styled } from "@mui/material/styles";
+
+const AppContainer = styled('div')({
+  backgroundColor: "#14161a",
+  color: "#fff",
+  height: "100vh",
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/coins/:id" element={<Coinpage />} />
-        </Routes>
-      </div>
-    </Router>
+    <StyledEngineProvider injectFirst>
+      <Router>
+        <AppContainer>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/coins/:id" element={<Coinpage />} />
+          </Routes>
+        </AppContainer>
+      </Router>
+    </StyledEngineProvider>
   );
 }
 
--- portfolio-tracker/src/components/Header.js
+++ portfolio-tracker/src/components/Header.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const Header = () => {
+    const [currency, setCurrency] = useState('USD');  // Setting a default value to 'USD'
+
+    const navigate = useNavigate();
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#fff',
+        },
+            mode: 'dark',
+        },
+    });
+
+    const handleCurrencyChange = (event) => {
+        setCurrency(event.target.value);
+    };
+
     return (
-        <div>
-            Header
-        </div>
-    )
+        <ThemeProvider theme={darkTheme}>
+        <AppBar color='transparent' position='static'>
+            <Container>
+                <Toolbar>
+                    <Typography onClick={() => navigate('/')}
+                        sx={{
+                            flex: 1,
+                            color: 'goldenrod',
+                            fontWeight: 'bold',
+                            cursor: 'pointer',
+                            fontFamily: 'Montserrat',
+                        }}
+                    >
+                        Crypto Kings
+                    </Typography>
+
+                    <Select 
+                        value={currency}
+                        onChange={handleCurrencyChange}
+                        variant='outlined'
+                        sx={{
+                            marginLeft: 1,  // Using MUI spacing units
+                            width: 100,
+                            height: 40,
+                            marginRight: 15,
+                        }}
+                    >
+                        <MenuItem value='USD'>USD</MenuItem>
+                        <MenuItem value='EUR'>EUR</MenuItem>
+                        <MenuItem value='GBP'>GBP</MenuItem>
+                        <MenuItem value='AUD'>AUD</MenuItem>
+                    </Select>
+                </Toolbar>
+            </Container>
+        </AppBar>
+        <CssBaseline />
+        </ThemeProvider>
+    );
 }
 
-export default Header
+export default Header;
