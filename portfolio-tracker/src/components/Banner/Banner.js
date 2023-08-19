2775c11640d9e1298812aaca8a69b6e9e7b106da
Freddy <freddydordoni@Mac-mini.local>
2023-08-19 14:43:28 +1000

    banner header done, now to render the carousel and use the API to get price action

--- portfolio-tracker/src/components/Banner/Banner.js
+++ portfolio-tracker/src/components/Banner/Banner.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Container, Typography } from "@mui/material";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div
+      sx={{  //! TODO : import of image not working
+        backgroundImage: "url(./images/banner.jpg)",
+      }}
+    >
+      <Container
+        sx={{  // container styling
+          height: 400,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          paddingTop: 25,
+        }}
+      >
+        <div
+          sx={{  // tagline styling
+            display: "flex",
+            height: "40%",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h2"  // font styling
+            sx={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat", // fixed the property name
+            }}
+          >
+            Crypto Kings
+          </Typography>
+          <Typography
+            variant="subtitle2"  // font styling for subtitle
+            sx={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat", // fixed the property name
+            }}
+          >
+            Keep up to date with the latest crypto price action
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
